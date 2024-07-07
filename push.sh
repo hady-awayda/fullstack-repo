a7a75cb11b221e6842360162cd1f401fd89e238f
Hady <hady.awayda@gmail.com>
2024-07-07 19:11:35 +0300

    Updated code

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 cd backend-repo
 git add .
 git commit -m "$1"
-git push
+git push origin main
 
 cd ../frontend-repo
 git add .
 git commit -m "$1"
-git push
+git push origin main
 
 cd ..
 git add .
